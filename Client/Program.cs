05fe58614f52649aecfb42014694025fc9303797
Rob Towell <rob.towell.development@live.co.uk>
2018-04-22 15:17:54 +0100

    update readme and fix indents

--- Client/Program.cs
+++ Client/Program.cs
@@ -13,71 +13,71 @@ namespace Client
 	class Program
 	{
 		static int Main(string[] args)
-        {
-            return RunMainAsync().Result;
-        }
+		{
+			return RunMainAsync().Result;
+		}
 
-        private static async Task<int> RunMainAsync()
-        {
-            try
-            {
-                using (var client = await StartClientWithRetries())
-                {
-                    await DoClientWork(client);
-                    Console.ReadKey();
-                }
+		private static async Task<int> RunMainAsync()
+		{
+			try
+			{
+				using (var client = await StartClientWithRetries())
+				{
+					await DoClientWork(client);
+					Console.ReadKey();
+				}
 
-                return 0;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                Console.ReadKey();
-                return 1;
-            }
-        }
+				return 0;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				Console.ReadKey();
+				return 1;
+			}
+		}
 
-        private static async Task<IClusterClient> StartClientWithRetries(int initializeAttemptsBeforeFailing = 5)
-        {
-            int attempt = 0;
-            IClusterClient client;
-            while (true)
-            {
-                try
-                {
-                    client = new ClientBuilder()
-                        .UseLocalhostClustering()
-                        .Configure<ClusterOptions>(options =>
-                        {
-                            options.ClusterId = "dev";
-                            options.ServiceId = "game";
-                        })
-                        .ConfigureLogging(logging => logging.AddConsole())
-                        .Build();
+		private static async Task<IClusterClient> StartClientWithRetries(int initializeAttemptsBeforeFailing = 5)
+		{
+			int attempt = 0;
+			IClusterClient client;
+			while (true)
+			{
+				try
+				{
+					client = new ClientBuilder()
+						.UseLocalhostClustering()
+						.Configure<ClusterOptions>(options =>
+						{
+							options.ClusterId = "dev";
+							options.ServiceId = "game";
+						})
+						.ConfigureLogging(logging => logging.AddConsole())
+						.Build();
 
-                    await client.Connect();
-                    Console.WriteLine("Client successfully connect to silo host");
-                    break;
-                }
-                catch (SiloUnavailableException)
-                {
-                    attempt++;
-                    Console.WriteLine($"Attempt {attempt} of {initializeAttemptsBeforeFailing} failed to initialize the Orleans client.");
-                    if (attempt > initializeAttemptsBeforeFailing)
-                    {
-                        throw;
-                    }
-                    await Task.Delay(TimeSpan.FromSeconds(4));
-                }
-            }
+					await client.Connect();
+					Console.WriteLine("Client successfully connect to silo host");
+					break;
+				}
+				catch (SiloUnavailableException)
+				{
+					attempt++;
+					Console.WriteLine($"Attempt {attempt} of {initializeAttemptsBeforeFailing} failed to initialize the Orleans client.");
+					if (attempt > initializeAttemptsBeforeFailing)
+					{
+						throw;
+					}
+					await Task.Delay(TimeSpan.FromSeconds(4));
+				}
+			}
 
-            return client;
-        }
+			return client;
+		}
 
-        private static async Task DoClientWork(IClusterClient client)
-        {
-            // TODO: Add Test client work
+		private static async Task DoClientWork(IClusterClient client)
+		{
+			// TODO: Add Test client work
 			Console.WriteLine("No work to do");
-        }
+		}
 	}
 }
--- Readme.md
+++ Readme.md
@@ -1,2 +1,12 @@
 # Orleans Game Project Example
 First attempt at writing a small sample game in Orleans
+
+## Prerequisite
+This project is built on .Net Core 2.0 with Orleans 2.0.
+In the future this project will use Docker and this will become a requirement.
+
+## Getting started
+1. Clone the project
+2. Open a terminal and run `dotnet restore` in the top level folder
+3. Open a second terminal, cd into `Server` folder, run `dotnet run`
+4. On original terminal, cd into `Client` folder, run `dotnet run`
